e6343d04bc027e66ef6bf07e13e2921ace617a9e
Dylan Gardham <message4dylan@gmail.com>
2023-06-25 09:59:57 +0100

    saves timezones per server to json

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::fs;
 use std::collections::BTreeMap;
-use std::sync::Arc;
+
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-use serenity::model::prelude::{GuildId};
+use serenity::model::prelude::GuildId;
 use serenity::prelude::*;
 
 use serde_json::Value;
+use serde::{Deserialize, Serialize};
 
-use chrono_tz::Tz;
-use chrono::{TimeZone, Utc, DateTime};
+use chrono_tz::{Tz, TZ_VARIANTS};
+use chrono::Utc;
 
 
 struct Handler;
@@ -50,22 +53,94 @@ impl FromVal for String {
     }
 }
 
-// static mut mapping: Arc<i32> = Arc::new(0);
+#[derive(Serialize, Deserialize)]
+struct Mapping {
+    data: Vec<(GuildId, Vec<(String, String)>)>
+}
 
-fn get_time_str(_guild_id: GuildId) -> String {
+lazy_static! {
+    static ref TEST: Mutex<BTreeMap<GuildId, Vec<(String, String)>>> = {
+        let m = BTreeMap::new();
+        let mutx = Mutex::new(m);
+
+        let zone_file = fs::read_to_string("zones.json");
+        let zones: Mapping;
+        match zone_file {
+            Ok(zone_file) => {
+                zones = match serde_json::from_str(&zone_file) {
+                    Ok(r) => {r},
+                    Err(_) => {
+                        println!("Failed to read from zones.json");
+                        // write_tzs();
+                        Mapping {data : Vec::new()}
+                    }
+                };
+            },
+            Err(_) => {
+                // write_tzs();
+                zones = Mapping {data : Vec::new()};
+                // write_tzs();
+                // panic!("Could not open zones.json: {}", e)
+            }
+        }
+
+        // let d: Mapping = 
+
+
+        for (guild, vec) in zones.data.iter() {
+            mutx.lock().unwrap().insert(*guild, vec.to_vec());
+        }
+        
+        // m.insert(GuildId(952651810546540606), vec![("the Americans".to_string(), "US/Eastern".to_string())]);
+        // m.insert(GuildId(952651810546540606), vec![("joey".to_string(), "Europe/London".to_string())]);
+
+        // Australian Eastern Standard Time (AEST)
+        mutx
+    };
+}
+
+fn write_tzs() {
+    let mut data: Vec<(GuildId, Vec<(String, String)>)> = Vec::new();
+    // TEST.lock().unwrap().
+    // let m: BTreeMap<GuildId, Vec<(String, String)>> = TEST.lock().unwrap();
+    for (key, val) in TEST.lock().unwrap().iter() {
+        data.push((*key, val.to_vec()))
+    }
+
+    for (guild, vec) in data.iter() {
+        TEST.lock().unwrap().insert(*guild, vec.to_vec());
+    }
+
+    fs::write("zones.json", serde_json::to_string(&Mapping { data : data}).unwrap()).unwrap();
+}
+
+fn get_time_str(guild_id: GuildId) -> String {
     let now = Utc::now();
     let mut output = "".to_string();
 
-
-    let usa_tz = "US/Eastern".parse::<Tz>().unwrap();
-    let usa_time = now.with_timezone(&usa_tz);
-    output += &format!("For the Americans it is currently {}", usa_time.format("%H:%M"));
-
-    let uk_time: chrono::DateTime<_> = now.with_timezone(&"Europe/London".parse::<Tz>().unwrap());
-    output += &format!("\nFor joey in the UK it is {}", uk_time.time().format("%H:%M"));
-    if usa_time.format("%d/%m/%y").to_string() != uk_time.format("%d/%m/%y").to_string() {
-        output += " the next day"
+    match TEST.lock().unwrap().get(&guild_id) {
+        Some(v) => {
+            for (who, timezone) in v.iter() {
+                output += &("\nfor ".to_string() + who + " it is ");
+                let who_time: chrono::DateTime<_> = now.with_timezone(&timezone.parse::<Tz>().unwrap());
+                output += &who_time.time().format("%H:%M").to_string()
+            }
+            
+        },
+        None => {
+            output += "No timezone data for this server, use /tzadd <who> <timezone>"
+        }
     }
+
+    // let usa_tz = "US/Eastern".parse::<Tz>().unwrap();
+    // let usa_time = now.with_timezone(&usa_tz);
+    // output += &format!("For the Americans it is currently {}", usa_time.format("%H:%M"));
+
+    // let uk_time: chrono::DateTime<_> = now.with_timezone(&"Europe/London".parse::<Tz>().unwrap());
+    // output += &format!("\nFor joey in the UK it is {}", uk_time.time().format("%H:%M"));
+    // if usa_time.format("%d/%m/%y").to_string() != uk_time.format("%d/%m/%y").to_string() {
+    //     output += " the next day"
+    // }
 
     output += "\nI'm a bot, message joey if something went wrong or needs changing";
     // let aus_time = ""
@@ -78,11 +153,33 @@ fn get_time_str(_guild_id: GuildId) -> String {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+
+        if msg.content.starts_with("/tzadd") {
+            let list: Vec<&str> = msg.content.split(" ").collect();
+            if list.len() != 3 {
+                if let Err(why) = msg.reply(&ctx.http, "usage: /tzadd person timezone").await {
+                    println!("Error sending message: {:?}", why);
+                }
+                return;
+            }
+
+            if ! TZ_VARIANTS.map(|tz| tz.to_string()).contains(&list[2].to_string()) {
+                if let Err(why) = msg.reply(&ctx.http, "Unknown timezone, full list comming soon").await {
+                    println!("Error sending message: {:?}", why);
+                }
+                return;
+            }
+
+            if !TEST.lock().unwrap().contains_key(&msg.guild_id.unwrap()) {
+                TEST.lock().unwrap().insert(msg.guild_id.unwrap(), Vec::new());
+            }
+            TEST.lock().unwrap().get_mut(&msg.guild_id.unwrap()).unwrap().push((list[1].to_string(), list[2].to_string()));
+            write_tzs();
+            if let Err(why) = msg.channel_id.say(&ctx.http, format!("Added {} to {}", list[1], list[2])).await {
                 println!("Error sending message: {:?}", why);
             }
         }
+
         else if ! msg.author.bot &&
                 msg.content.to_ascii_lowercase().contains("time") &&
                 msg.content.to_ascii_lowercase().contains("zone") {
@@ -107,6 +204,10 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    // for tz in chrono_tz::TZ_VARIANTS {
+    //     println!("{}", tz);
+    // }
+
     let conf = get_conf();
     let token = String::get_config(&"APIKey", conf.await);
     
